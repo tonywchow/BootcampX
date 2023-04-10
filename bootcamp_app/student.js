3e1d1b42f9cbfdaaa3e6318dc20ccbbb89d3355f
Tony Chow <tchow9@gmail.com>
2023-04-09 20:14:24 -0700

    Sanitizing inputs

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -12,22 +12,26 @@ client.connect().then(() => {
   console.log("Connected to database!");
 });
 
-client
-  .query(
-    `
-SELECT students.id as id, students.name as name, cohorts.name as cohort
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`
-  )
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+//Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+client
+  .query(queryString, values)
   .then((res) => {
     // console.log(res.rows);
     res.rows.forEach((user) => {
       console.log(
-        `${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
       );
     });
   })
-  .catch((err) => console.error("query error", err));
+  .catch((err) => console.error("query error", err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -12,16 +12,21 @@ client.connect().then(() => {
   console.log("Connected to database!");
 });
 
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`;
+
+const cohortName = process.argv[2] || "JUL02";
+//Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
 client
-  .query(
-    `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-    FROM assistance_requests
-    JOIN teachers ON teachers.id = teacher_id
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '%${process.argv[2] || "JUL02"}%'
-    ORDER BY teacher;`
-  )
+  .query(queryString, values)
   .then((res) => {
     // console.log(res.rows);
     res.rows.forEach((row) => {
